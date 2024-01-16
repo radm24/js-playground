92d7a1414e963bf159647f702b82ccf3c77a1bee
radm24 <r.musin24@mail.ru>
2024-01-16 23:31:24 +0300

    adding quote generator app

---
+++ quote-generator/assets/js/script.js
@@ -0,0 +1,84 @@
+'use strict';
+
+(function quoteGenerator() {
+  let apiQuotes = [];
+  let quoteNumber = 0;
+  const quotesLimit = 50;
+  const fallbackQuote = {
+    quote: 'You must be the change you wish to see in the world.',
+    author: 'Mahatma Gandhi',
+  };
+
+  const getElBySelector = (selector) => {
+    return document.querySelector(selector);
+  };
+
+  const quoteContainer = getElBySelector('#quote-container');
+  const quoteText = getElBySelector('#quote');
+  const authorText = getElBySelector('#author');
+  const twitterBtn = getElBySelector('#twitter');
+  const newQuoteBtn = getElBySelector('#new-quote');
+  const loader = getElBySelector('#loader');
+
+  const loading = () => {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+  };
+
+  const complete = () => {
+    loader.hidden = true;
+    quoteContainer.hidden = false;
+  };
+
+  const printQuote = (quote) => {
+    loading();
+    const quoteLen = quote['quote'].length;
+    // Reduce font size for long quotes
+    if (quoteLen > 120) {
+      quoteText.classList.add('long-quote');
+    } else {
+      quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.quote;
+    authorText.textContent = quote.author;
+    complete();
+  };
+
+  const getQuotes = () => {
+    loading();
+    // Total: 1454 quotes
+    const skip = Math.floor(Math.random() * 1405);
+    const apiUrl = `https://dummyjson.com/quotes?limit=${quotesLimit}&skip=${skip}`;
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        apiQuotes = data.quotes;
+        printQuote(apiQuotes[0]);
+      })
+      .catch((error) => {
+        console.log('whoops, no quote', error);
+        quoteNumber = quotesLimit - 1;
+        printQuote(fallbackQuote);
+      });
+  };
+
+  const tweetQuote = () => {
+    const { quote, author } = { ...apiQuotes[quoteNumber] };
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+  };
+
+  const printNewQuote = () => {
+    quoteNumber++;
+    if (quoteNumber === quotesLimit) {
+      quoteNumber = 0;
+      getQuotes();
+    } else {
+      printQuote(apiQuotes[quoteNumber]);
+    }
+  };
+
+  twitterBtn.addEventListener('click', tweetQuote);
+  newQuoteBtn.addEventListener('click', printNewQuote);
+  getQuotes();
+})();
