3569b735ab155e11fd1a7eb4f2d462c93006d81d
radm24 <r.musin24@mail.ru>
2024-01-26 06:00:36 +0300

    adding responsive, favicon; refactoring (light-dark-mode)

--- 07-light-dark-mode/assets/js/script.js
+++ 07-light-dark-mode/assets/js/script.js
@@ -1,37 +1,12 @@
 'use strict';
 
 (function lightDarkMode() {
-  const themeProps = {
-    light: {
-      html: ['data-theme', 'light'],
-      'header-background-color': 'rgb(255 255 255 / 50%)',
-      'theme-name': 'Light Mode',
-      'schedule-section-img-sources': [
-        './assets/img/undraw_yacht_light.svg',
-        './assets/img/undraw_map_light.svg',
-        './assets/img/undraw_beer_light.svg',
-      ],
-      'text-box-background-color': 'rgb(0 0 0 / 50%)',
-    },
-    dark: {
-      html: ['data-theme', 'dark'],
-      'header-background-color': 'rgb(0 0 0 / 50%)',
-      'theme-name': 'Dark Mode',
-      'schedule-section-img-sources': [
-        './assets/img/undraw_yacht_dark.svg',
-        './assets/img/undraw_map_dark.svg',
-        './assets/img/undraw_beer_dark.svg',
-      ],
-      'text-box-background-color': 'rgb(255 255 255 / 50%)',
-    },
-  };
-
   const getElBySelector = (selector) => document.querySelector(selector);
 
   const htmlEl = getElBySelector('html');
   const headerEl = getElBySelector('.header');
+  const themeText = getElBySelector('#theme-name');
   const toggleSwitchCheckbox = getElBySelector('#theme-switch input');
-  const toggleText = getElBySelector('#theme-name');
   const sunIcon = getElBySelector('#sun-icon');
   const moonIcon = getElBySelector('#moon-icon');
   const image1 = getElBySelector('#image1');
@@ -40,22 +15,27 @@
   const textBox = getElBySelector('#text-box');
 
   const switchTheme = (e) => {
-    const props = e.target.checked ? themeProps['dark'] : themeProps['light'];
+    const mode = e.target.checked ? 'dark' : 'light';
+    const headerBackgroundColor =
+      mode === 'dark' ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+    const textBoxBackgroundColor =
+      mode === 'dark' ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+
     // Set theme colors
-    htmlEl.setAttribute(...props.html);
+    htmlEl.setAttribute('data-theme', mode);
     // Change header background color
-    headerEl.style.backgroundColor = props['header-background-color'];
-    // Change theme name
-    toggleText.textContent = props['theme-name'];
+    headerEl.style.backgroundColor = headerBackgroundColor;
+    // Change theme name (theme's first letter capitalization is made in css file)
+    themeText.textContent = `${mode} Mode`;
     // Change theme icon
     sunIcon.classList.toggle('hidden');
     moonIcon.classList.toggle('hidden');
     // Change image sources of schedule section
-    [image1, image2, image3].forEach((img, idx) => {
-      img.src = props['schedule-section-img-sources'][idx];
-    });
+    image1.src = `./assets/img/undraw_yacht_${mode}.svg`;
+    image2.src = `./assets/img/undraw_map_${mode}.svg`;
+    image3.src = `./assets/img/undraw_beer_${mode}.svg`;
     // Change background color of text-box in 'hall of fame' section
-    textBox.style.backgroundColor = props['text-box-background-color'];
+    textBox.style.backgroundColor = textBoxBackgroundColor;
   };
 
   toggleSwitchCheckbox.addEventListener('change', switchTheme);
