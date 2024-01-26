490959511b21235dbbd72e0836cefac931157ed7
radm24 <r.musin24@mail.ru>
2024-01-26 07:23:31 +0300

    save user's theme choice in localStorage (light-dark-mode)

--- 07-light-dark-mode/assets/js/script.js
+++ 07-light-dark-mode/assets/js/script.js
@@ -15,28 +15,40 @@
   const textBox = getElBySelector('#text-box');
 
   const switchTheme = (e) => {
-    const mode = e.target.checked ? 'dark' : 'light';
+    const theme = e.target.checked ? 'dark' : 'light';
     const headerBackgroundColor =
-      mode === 'dark' ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
+      theme === 'dark' ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)';
     const textBoxBackgroundColor =
-      mode === 'dark' ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
+      theme === 'dark' ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)';
 
+    // Save theme value in localStorage
+    localStorage.setItem('theme', theme);
     // Set theme colors
-    htmlEl.setAttribute('data-theme', mode);
+    htmlEl.setAttribute('data-theme', theme);
     // Change header background color
     headerEl.style.backgroundColor = headerBackgroundColor;
     // Change theme name (theme's first letter capitalization is made in css file)
-    themeText.textContent = `${mode} Mode`;
+    themeText.textContent = `${theme} Mode`;
     // Change theme icon
     sunIcon.classList.toggle('hidden');
     moonIcon.classList.toggle('hidden');
     // Change image sources of schedule section
-    image1.src = `./assets/img/undraw_yacht_${mode}.svg`;
-    image2.src = `./assets/img/undraw_map_${mode}.svg`;
-    image3.src = `./assets/img/undraw_beer_${mode}.svg`;
+    image1.src = `./assets/img/undraw_yacht_${theme}.svg`;
+    image2.src = `./assets/img/undraw_map_${theme}.svg`;
+    image3.src = `./assets/img/undraw_beer_${theme}.svg`;
     // Change background color of text-box in 'hall of fame' section
     textBox.style.backgroundColor = textBoxBackgroundColor;
   };
 
+  const checkUserTheme = () => {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+      toggleSwitchCheckbox.checked = true;
+      toggleSwitchCheckbox.dispatchEvent(new Event('change'));
+    }
+  };
+
   toggleSwitchCheckbox.addEventListener('change', switchTheme);
+  // Check localStorage for theme item
+  checkUserTheme();
 })();
