a7b5ff254c433f507c3f6e533f01f8471b726a9e
radm24 <r.musin24@mail.ru>
2024-01-27 23:57:46 +0300

    adding animated-template project

---
+++ 08-animated-template/assets/js/navbar.js
@@ -0,0 +1,10 @@
+(function () {
+  const toggleNavbar = (collapseID) => {
+    document.getElementById(collapseID).classList.toggle('hidden');
+    document.getElementById(collapseID).classList.toggle('block');
+  };
+
+  document
+    .getElementById('menu-button')
+    .addEventListener('click', () => toggleNavbar('example-collapse-navbar'));
+})();
