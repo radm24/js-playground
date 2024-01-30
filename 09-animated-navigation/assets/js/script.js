c6dd89ba7058adc6a775a532f6df03c6ad4feed4
radm24 <r.musin24@mail.ru>
2024-01-30 15:15:40 +0300

    adding animated-navigation functionality

--- 09-animated-navigation/assets/js/script.js
+++ 09-animated-navigation/assets/js/script.js
@@ -1,5 +1,25 @@
 'use strict';
 
 (function animatedNavigation() {
-  // placeholder
+  const overlay = document.getElementById('overlay');
+  const menuIcon = document.getElementById('menu-icon');
+  const navListItems = document.querySelectorAll('.nav-list li');
+  const navListLinks = document.querySelectorAll('.nav-link');
+
+  const toggleNav = () => {
+    // Toggle menu icon appearance
+    menuIcon.classList.toggle('change');
+    // Show/hide navigation menu overlay
+    overlay.classList.toggle('overlay-active');
+    // Toggle animation for navigation menu items
+    navListItems.forEach((item, idx) => {
+      item.classList.toggle(`slide-in-${idx + 1}`);
+      item.classList.toggle(`slide-out-${idx + 1}`);
+    })
+  }
+
+  menuIcon.addEventListener('click', toggleNav);
+  navListLinks.forEach(link => {
+    link.addEventListener('click', toggleNav);
+  })
 })();
